efc5163eea4b2c3093d632c3665cacea1bcedca5
Dvir Volk <dvirsky@gmail.com>
2016-05-27 13:33:01 +0300

    added unit test for search client

--- govsearch/engine/__init__.py
+++ govsearch/engine/__init__.py
@@ -1,5 +1,6 @@
 from redis import Redis, RedisError, ConnectionPool
 import datetime
+import itertools
 
 class Item(object):
 
@@ -25,6 +26,10 @@ class Document(object):
         for k,v in fields.iteritems():
             setattr(self, k, v)
 
+    def __repr__(self):
+
+        return 'Document %s' % self.__dict__
+
 
 class Result(object):
 
@@ -34,7 +39,7 @@ class Result(object):
         self.docs = []
         for i in xrange(1, len(res), 2 if hascontent else 1):
             id = res[i]
-            fields = dict(res[i+1]) if hascontent else {}
+            fields = dict(dict(itertools.izip(res[i+1][::2], res[i+1][1::2]))) if hascontent else {}
             self.docs.append(Document(id, **fields))
 
 class SearchClient(object):
@@ -59,7 +64,8 @@ class SearchClient(object):
         :param fields: a kwargs consisting of field=[score|NUMERIC]
         :return:
         """
-        self.redis.execute_command(self.CREATE_CMD, self.index_name, *fields.items())
+        print fields.items()
+        self.redis.execute_command(self.CREATE_CMD, self.index_name, *itertools.chain(*fields.items()))
 
     def drop_index(self):
         """
@@ -68,8 +74,11 @@ class SearchClient(object):
         """
         self.redis.execute_command(self.DROP_CMD, self.index_name)
 
-    def add_document(self, **fields):
-        pass
+    def add_document(self, doc_id, score=1.0, **fields):
+
+        args = [self.ADD_CMD, self.index_name, doc_id, score, 'FIELDS'] + list(itertools.chain(*fields.items()))
+        self.redis.execute_command(*args
+                                   )
 
     def search(self, query, no_content = False, fields=None, **filters):
         """
@@ -93,7 +102,6 @@ class SearchClient(object):
         if filters:
             for k, v in filters.iteritems():
               args += ['FILTER', k] + list(v)
-        print args
 
         res = self.redis.execute_command(self.SEARCH_CMD, *args)
 
---
+++ govsearch/test/test_engine.py
@@ -0,0 +1,27 @@
+from govsearch.engine import *
+import unittest
+
+class EngineTestCase(unittest.TestCase):
+
+    def testEngine(self):
+
+        client = SearchClient('testung')
+        client.drop_index()
+        client.create_index(title=10, body=1, foo=client.NUMERIC)
+
+        client.add_document(doc_id="doc1", title="hello world", body="foo bar hello", foo = 1)
+        client.add_document(doc_id="doc2", title="hello my world", body="foo bar helloz", foo = 2)
+
+        res = client.search("hello world")
+
+        self.assertEqual(2, res.total)
+        self.assertEqual(2, len(res.docs))
+        print res.docs
+
+        res = client.search('hello', foo = ('-inf',1))
+        self.assertEqual(1, res.total)
+        self.assertEqual(1, len(res.docs))
+
+        res = client.search('hello', fields=['body'])
+        self.assertEqual(1, res.total)
+        self.assertEqual(1, len(res.docs))
