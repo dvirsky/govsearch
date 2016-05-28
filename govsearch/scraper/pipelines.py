de4df91d4121b53e272ddab0dcbdf5f05fa31bd9
Ory Band <oryband@gmail.com>
2016-05-28 11:17:33 +0300

    scraper dumps json error objects instead of crashing

--- govsearch/scraper/pipelines.py
+++ govsearch/scraper/pipelines.py
@@ -4,6 +4,13 @@ import arrow
 import json
 import re
 
+class ResolutionError(RuntimeError):
+    """Raised when crawling resulted in unexpected results
+
+    e.g. multiple titles, empty bodies, etc.
+    """
+    pass
+
 
 class ResolutionPipeline(object):
     def __init__(self):
@@ -20,20 +27,26 @@ class ResolutionPipeline(object):
         self.gov_pattern = re.compile(r'^.+\s??\-\s?(?P<gov_number>.+?)\s+?(?P<pm_name>.+?);?$')
 
     def process_item(self, item, spider):
-        data = {
-            'url': item["url"],
-            'date': self.get_date(item).timestamp,
-            'resolution_number': self.get_resolution_number(item),
-            'gov_number': self.get_gov_number(item),
-            'pm_name': self.get_pm_name(item),
-            'title': self.get_title(item),
-            'subject': self.get_subject(item),
-            'body': self.get_body(item),
-        }
+        try:
+            data = {
+                'url': item["url"],
+                'date': self.get_date(item).timestamp,
+                'resolution_number': self.get_resolution_number(item),
+                'gov_number': self.get_gov_number(item),
+                'pm_name': self.get_pm_name(item),
+                'title': self.get_title(item),
+                'subject': self.get_subject(item),
+                'body': self.get_body(item),
+            }
+        except ResolutionError as e:
+            dump = (json.dumps({'error': repr(e), 'url': item["url"]},
+                               ensure_ascii=False)
+                    .encode("utf8"))
+        else:
+            # encode to utf-8 and dump to file
+            dump = json.dumps(data, ensure_ascii=False).encode("utf8")
 
-        # encode to utf-8 and dump to file
-        resolution = json.dumps(data, ensure_ascii=False).encode("utf8")
-        self.file.write(resolution + "\n")
+        self.file.write(dump + "\n")
 
         return item
 
@@ -43,37 +56,37 @@ class ResolutionPipeline(object):
 
     def get_date(self, item):
         if len(item["date"]) != 1:
-            raise RuntimeError("Date field length is not 1 for item %s", item)
+            raise ResolutionError("Date field length is not 1 for item %s", item)
         return arrow.get(item["date"][0], "YYYYMMDD")
 
     def get_resolution_number(self, item):
         if len(item["resolution_number"]) != 1:
-            raise RuntimeError("Resolution number field length is not 1 for item %s", item)
+            raise ResolutionError("Resolution number field length is not 1 for item %s", item)
         return self.resolution_number_pattern.search(item["resolution_number"][0]).group('number')
 
     def get_gov_number(self, item):
         if len(item["gov"]) != 1:
-            raise RuntimeError("Government field length is not 1 for item %s", item)
+            raise ResolutionError("Government field length is not 1 for item %s", item)
         gov_match = self.gov_pattern.search(item["gov"][0])
         return gov_match.group("gov_number")
 
     def get_pm_name(self, item):
         if len(item["gov"]) != 1:
-            raise RuntimeError("Government field length is not 1 for item %s", item)
+            raise ResolutionError("Government field length is not 1 for item %s", item)
         gov_match = self.gov_pattern.search(item["gov"][0])
         return gov_match.group("pm_name")
 
     def get_title(self, item):
         if len(item["title"]) == 0:
-            raise RuntimeError("Title fields is empty for item %s", item)
+            raise ResolutionError("Title fields is empty for item %s", item)
         return ''.join(item["title"]).strip()
 
     def get_subject(self, item):
         if len(item["subject"]) == 0:
-            raise RuntimeError("Subject field is empty for item %s", item)
+            raise ResolutionError("Subject field is empty for item %s", item)
         return ''.join(item["subject"]).strip()
 
     def get_body(self, item):
         if len(item["body"]) == 0:
-            raise RuntimeError("Body field is empty for item %s", item)
+            raise ResolutionError("Body field is empty for item %s", item)
         return ''.join(item["body"]).strip()
